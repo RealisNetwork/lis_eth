d971f5cc9ed9a9093df498e5a30757ce311bcb11
prometeiii <prometeii1998@gmail.com>
2023-05-15 13:55:42 +0300

    Added roles to LIS, chai package

--- contracts/Lis.sol
+++ contracts/Lis.sol
@@ -3,9 +3,24 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Lis is ERC20 {
+contract Lis is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
     constructor(uint256 initialSupply) ERC20("Realis", "LIS") {
         _mint(msg.sender, initialSupply);
+        // Grant the contract deployer the default admin role: it will be able
+        // to grant and revoke any roles
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
     }
 }
