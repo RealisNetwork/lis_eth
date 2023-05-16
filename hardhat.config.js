a07d3e4bdf1a6561c668515196e69f85ebd4b7e5
prometeiii <prometeii1998@gmail.com>
2023-05-16 17:40:48 +0300

    updated config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,15 @@ require('@nomiclabs/hardhat-ethers');
 require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
     goerly: {
       url: `https://eth-goerli.alchemyapi.io/v2/${process.env.INFURA_PROJECT_ID}`,
@@ -13,14 +21,14 @@ module.exports = {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [process.env.PRIVATE_KEY],
     },
-  },
-  solidity: {
-    version: '0.8.9',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+    localhost: {
+      url: "http://127.0.0.1:8545"
     },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
   },
 };
