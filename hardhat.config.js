6651fcf098d25736835376b5b4d0faa9246afc59
prometeiii <prometeii1998@gmail.com>
2023-05-16 18:08:03 +0300

    added polygon main to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,6 +21,10 @@ module.exports = {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [process.env.PRIVATE_KEY],
     },
+    polygon_main: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
     localhost: {
       url: "http://127.0.0.1:8545"
     },
