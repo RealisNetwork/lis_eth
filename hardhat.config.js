27b825a8b36c934661b725793891900e8f7a2ef4
prometeiii <prometeii1998@gmail.com>
2023-06-15 16:28:49 +0300

    Added opensea drop contract, packages and git submodules.

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,14 @@ require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
   solidity: {
-    version: "0.8.18",
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.8.18",
+      },
+    ],
     settings: {
       optimizer: {
         enabled: true,
