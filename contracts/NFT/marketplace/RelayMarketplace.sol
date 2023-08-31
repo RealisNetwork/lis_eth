eb949562139a62c357ad89ea303a400ab381e796
vladislv31 <vladislv.k31@gmail.com>
2023-08-31 12:47:22 +0300

    relay

---
+++ contracts/NFT/marketplace/RelayMarketplace.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/GSN/GSNRecipient.sol";
+
+contract RelayMarketplace is GSNRecipient {
+    using SafeMath for uint256;
+}
